a643c8a2966c68691cd0930c8924d6c715103bea
sigevsky <nick.sigevsky@gmail.com>
2024-09-07 19:55:09 +0300

    Track op/tier

--- config/proxy-monitor/proxy-monitor.py
+++ config/proxy-monitor/proxy-monitor.py
@@ -14,12 +14,13 @@ from requests_toolbelt.adapters import source
 # Configuration
 API_KEY = "0a7243c9-13f8-4959-aedf-69584fefda8c"
 AGENT_ID = "e895f776-e5e0-41f6-906f-aa45fe5dfc40"
+topology = "{{ topology }}"
+nid = int("{{ nid }}")
 
 # API_KEY = "f388a0a8-13f1-44cc-b72d-eed08f6adffb"
 # AGENT_ID = "8513f528-11ad-430f-8f09-68aa5fe6ca4e"
-
-topology = "{{ topology }}"
-nid = int("{{ nid }}")
+# topology = "location"
+# nid = 161
 
 API_HEADERS = {
     "Accept": "*/*",
@@ -29,19 +30,21 @@ API_HEADERS = {
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-public_ip_gauge = Gauge('public_ip', 'Public IP address for each interface', ['local_ip', 'server_name', 'bind'])
+labelnames = ['local_ip', 'server_name', 'bind', 'operator', 'tier']
+
+public_ip_gauge = Gauge('public_ip', 'Public IP address for each interface', labelnames)
 ip_check_duration = Summary('ip_check_duration_seconds', 'Time taken to check public IP',
-                            ['server_name', 'bind'])
-successful_checks = Counter('successful_ip_checks_total', 'Total number of successful IP checks',
-                            ['local_ip', 'server_name', 'bind'])
+                            labelnames)
+successful_checks = Counter('successful_ip_checks_total', 'Total number of successful IP checks', labelnames)
 timeout_errors = Counter('ip_check_timeout_errors_total', 'Total number of timeout errors during IP checks',
-                         ['local_ip', 'server_name', 'bind'])
+                         labelnames)
 other_errors = Counter('ip_check_other_errors_total', 'Total number of non-timeout errors during IP checks',
-                       ['local_ip', 'server_name', 'bind'])
+                       labelnames)
 
 
 def format_virtual_address(instance_ord: int, location_ord: int, device_ord: int) -> str:
     return f"10.{instance_ord}.{location_ord}.{device_ord}"
+
 
 def fetch_agent_instances() -> list:
     try:
@@ -77,7 +80,12 @@ def fetch_bind_devices() -> list:
             {
                 "addr": device["bindTarget"],
                 "server_name": device["location"]["name"],
-                "bind": "local"
+                "bind": "local",
+                "operator": next(
+                    (t["value"] for t in devices[0]["tags"] if t["value"] in ["vodafone", "kyivstar", "life"]),
+                    "unknown"),
+                "tier": next((t["value"] for t in devices[0]["tags"] if t["value"] in ["standard", "premium"]),
+                             "unknown"),
             }
             for device in devices if device["location"]["id"] == nid
         ]
@@ -93,7 +101,12 @@ def fetch_bind_devices() -> list:
                 "addr": format_virtual_address(current_instance["ordinal"], device["location"]["ordinal"],
                                                device["ordinal"]),
                 "server_name": device["location"]["name"],
-                "bind": current_instance["name"]
+                "bind": current_instance["name"],
+                "operator": next(
+                    (t["value"] for t in devices[0]["tags"] if t["value"] in ["vodafone", "kyivstar", "life"]),
+                    "unknown"),
+                "tier": next((t["value"] for t in devices[0]["tags"] if t["value"] in ["standard", "premium"]),
+                             "unknown"),
             }
             for device in devices
         ]
@@ -101,8 +114,8 @@ def fetch_bind_devices() -> list:
         raise ValueError(f"Unsupported topology: {topology}")
 
 
-def execute_call(source_ip: str, server_name: str, bind: bool) -> str:
-    with ip_check_duration.labels(server_name=server_name, bind=bind).time():
+def execute_call(source_ip: str, server_name: str, bind: str, tier: str, operator: str) -> str:
+    with ip_check_duration.labels(local_ip=source_ip, server_name=server_name, bind=bind, operator=operator, tier=tier).time():
         with requests.Session() as session:
             # disable retries
             adapter = HTTPAdapter(max_retries=0, pool_connections=1, pool_maxsize=1)
@@ -115,43 +128,49 @@ def execute_call(source_ip: str, server_name: str, bind: bool) -> str:
                 response = session.get('https://api.ipify.org/?format=text', timeout=5)
                 public_ip = response.text.strip()
                 logger.info(f"Successfully retrieved public IP for {source_ip}: {public_ip}")
-                successful_checks.labels(local_ip=source_ip, server_name=server_name, bind=bind).inc()
+                successful_checks.labels(local_ip=source_ip, server_name=server_name, bind=bind, tier=tier,
+                                         operator=operator).inc()
                 return public_ip
             except requests.Timeout:
                 logger.error(f"Timeout error while getting public IP for {source_ip}")
-                timeout_errors.labels(local_ip=source_ip, server_name=server_name, bind=bind).inc()
+                timeout_errors.labels(local_ip=source_ip, server_name=server_name, bind=bind, tier=tier,
+                                      operator=operator).inc()
                 return f"Timeout error for {source_ip}"
             except requests.RequestException as e:
                 logger.error(f"Failed to get public IP for {source_ip}. Error: {str(e)}")
-                other_errors.labels(local_ip=source_ip, server_name=server_name, bind=bind).inc()
+                other_errors.labels(local_ip=source_ip, server_name=server_name, bind=bind, tier=tier,
+                                    operator=operator).inc()
                 return f"Failed to get public IP for {source_ip}"
 
 
 def main():
-        start_http_server(7878)
-        logger.info("Prometheus metrics server started on port 7878")
-        devices = fetch_bind_devices()
-        logger.info(f"Devices: {devices}")
+    start_http_server(7878)
+    logger.info("Prometheus metrics server started on port 7878")
+    devices = fetch_bind_devices()
+    logger.info(f"Devices: {devices}")
 
-        while True:
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                futures = {
-                    executor.submit(execute_call, device['addr'], device["server_name"], device["bind"]): device
-                    for device in
-                    devices}
+    while True:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = {
+                executor.submit(execute_call, device['addr'], device["server_name"], device["bind"], device["tier"],
+                                device["operator"]): device
+                for device in devices
+            }
 
-                for future in concurrent.futures.as_completed(futures):
-                    device = futures[future]
-                    public_ip = future.result()
+            for future in concurrent.futures.as_completed(futures):
+                device = futures[future]
+                public_ip = future.result()
 
-                    if not public_ip.startswith("Failed") and not public_ip.startswith("Timeout"):
-                        public_ip_gauge.labels(
-                            local_ip=device['addr'],
-                            server_name=device["server_name"],
-                            bind=device["bind"]
-                        ).set(hash(public_ip))
+                if not public_ip.startswith("Failed") and not public_ip.startswith("Timeout"):
+                    public_ip_gauge.labels(
+                        local_ip=device['addr'],
+                        server_name=device["server_name"],
+                        bind=device["bind"],
+                        operator=device["operator"],
+                        tier=device["tier"]
+                    ).set(hash(public_ip))
 
-            time.sleep(8)
+        time.sleep(8)
 
 
 if __name__ == "__main__":
